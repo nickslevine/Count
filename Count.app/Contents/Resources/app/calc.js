469da7a85beac262f266bbeb22e4324eca842b4b
Nick Levine <nickslevine@gmail.com>
2017-03-25 19:11:28 +0000

    changes

--- Count.app/Contents/Resources/app/calc.js
+++ Count.app/Contents/Resources/app/calc.js
@@ -1,15 +1,15 @@
 //Interface
 function calc() {
         if (document.getElementById('expression').value != '') {
-        exp = document.getElementById('expression').value;
-        ans = eval(exp);
-        console.log(exp, " ", ans);
+            exp = document.getElementById('expression').value;
+            ans = eval(exp);
+            console.log(exp, " ", ans);
         if (ans != undefined) {
-        document.getElementById('answers').innerHTML+= exp;
-        document.getElementById('answers').innerHTML+= '<span class="answertext">\t=\t';
-        document.getElementById('answers').innerHTML+= '<b>' + ans + '</b></span>';
-        document.getElementById('answers').innerHTML+= '<br><br>';
-        document.getElementById('expression').value = ans;
+            document.getElementById('answers').innerHTML+= exp;
+            document.getElementById('answers').innerHTML+= '<span class="answertext">\t=\t';
+            document.getElementById('answers').innerHTML+= '<b>' + ans + '</b></span>';
+            document.getElementById('answers').innerHTML+= '<br><br>';
+            document.getElementById('expression').value = ans;
         } else {
             document.getElementById('expression').value = '';
         }
@@ -38,3 +38,19 @@ function calc() {
     let pi = Math.PI;
 
     let sum = (...nums) => [...nums].reduce((acc, val) => (acc + val));
+
+    let mean = (...nums) => [...nums].reduce((p, e) => {
+        return p + e;
+    }) / nums.length;
+
+    let c = (n) => ((n - 32) * 5 / 9);
+
+    let f = (n) => (n * (9 / 5) + 32);
+
+    let roots = (a,b,c) => {
+        let arr = [];
+        arr.push(((-1*b + (sqrt(b*b - 4*a*c)))/2*a));
+        arr.push(((-1*b - (sqrt(b*b - 4*a*c)))/2*a));
+        return arr;
+    }
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Count
+
+## Functions
+
+* c(number): convert to Fahrenheit
+* f(number): convert to Celsius
+* sqrt(number): take square root
+* sum(numbers): add numbers
+* mean(numbers): get average of numbers
+* roots(a, b, c): get roots of quadratic function
+
+
+## Built-in Variables
+* pi
+
+## To Add
+[ ] Convert hex to rgb and vice-versa
+[ ] Array manipulation
+[ ] Date manipulation
+[ ] up and down arrow to access history
+[ ] function to list variables in current scope
+[ ] d3 charts
+[ ] sounds
+[ ] help
+[ ] random numbers
